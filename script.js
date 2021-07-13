892667d5c14a40e6c02294a07a01951ad8f645a9
Kevin <kevjiao@dunlapstudents.net>
2021-07-13 08:45:36 -0500

    alphabetical order

---
+++ script.js
@@ -0,0 +1,34 @@
+  
+function chooseCountry() {
+    var chosenCountry = countries[Math.floor(Math.random()*countries.length)]
+    console.log(chosenCountry)
+    console.log(countries.length)
+    return chosenCountry
+}
+
+function choices(countries, chosenCountry) {
+
+}
+
+var countries = [
+    "Afganistan",
+    "Australia", 
+    "Brasil", 
+    "Belgium",
+    "Canada", 
+    "China",
+    "Egypt", 
+    "France",
+    "Germany",  
+    "Greenland",
+    "Iceland",
+    "India",
+    "Italy",
+    "Japan", 
+    "Luxembourg", 
+    "Mexico",
+    "Russia",
+    "Spain",  
+    "United Kingdom", 
+    "United States", 
+]
